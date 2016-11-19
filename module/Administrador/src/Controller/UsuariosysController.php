6354b5d86b211f2c76f87a31c552fe36535e004b
Jackson <jackson@jacksonveroneze.com>
2016-11-19 10:44:33 -0200

    Tarefa: refs #13923 Descrição: Adicionado suporte a migrations.

--- module/Administrador/src/Controller/UsuariosysController.php
+++ module/Administrador/src/Controller/UsuariosysController.php
@@ -7,9 +7,31 @@ use \Zend\Mvc\Controller\AbstractRestfulController;
 class UsuariosysController extends AbstractRestfulController
 {
 
+    public function create($data)
+    {
+        return new \Zend\View\Model\JsonModel(parent::create($data));
+    }
+
+    public function delete($id)
+    {
+        parent::delete($id);
+    }
+
+    public function get($id)
+    {
+        parent::get($id);
+    }
+
     public function getList()
     {
-        return new \Zend\View\Model\JsonModel(['1']);
+         return new \Zend\View\Model\JsonModel(parent::getList($data));
+        
+        parent::getList();
+    }
+
+    public function update($id, $data)
+    {
+        parent::update($id, $data);
     }
 
 }
