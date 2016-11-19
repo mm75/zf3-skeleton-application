6b565fb0d8236900d7c2e960c10e8181ed883bca
Jackson <jackson@jacksonveroneze.com>
2016-11-19 09:02:23 -0200

    Tarefa: refs #13923 Descrição: Removido arquivos da camada VIEW do módulo administrador.

--- module/Administrador/config/module.config.php
+++ module/Administrador/config/module.config.php
@@ -2,43 +2,24 @@
 
 namespace Administrador;
 
-use Zend\Router\Http\Literal;
-use Zend\ServiceManager\Factory\InvokableFactory;
-
 return [
     'router' => [
         'routes' => [
             'administrador' => [
-                'type' => Literal::class,
+                'type' => 'segment',
                 'options' => [
-                    'route' => '/administrador',
-                    'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action' => 'index',
-                    ],
-                ],
+                    'route' => '/administrador[:controller[/:id]]',
+                    'constraints' => [
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ]
+                ]
             ]
         ],
     ],
     'controllers' => [
         'factories' => [
-            Controller\IndexController::class => InvokableFactory::class,
-        ],
-    ],
-    'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
-        'template_map' => [
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'administrador/index/index' => __DIR__ . '/../view/administrador/index/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
-        ],
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
-    ],
+            Controller\IndexController::class => \Zend\ServiceManager\Factory\InvokableFactory::class,
+        ]
+    ]
 ];
