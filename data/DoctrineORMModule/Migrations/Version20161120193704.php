cbe155c105070fbeff7e6bb6a3b3184aadbc377b
Jackson <jackson@jacksonveroneze.com>
2016-11-20 19:39:03 -0200

    Tarefa: refs #13923 Descrição: diretório para armazenar as proxies e ajustes gerais.

---
+++ data/DoctrineORMModule/Migrations/Version20161120193704.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161120193704 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE usuario_sys (id SERIAL NOT NULL, usuario VARCHAR(10) NOT NULL, nome VARCHAR(50) DEFAULT NULL, senha VARCHAR(100) DEFAULT NULL, ativo BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_E2BF03B12265B05D ON usuario_sys (usuario)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE usuario_sys');
+    }
+}
